3fde1ba553db265e3ffa8f06a32a5f9e1827677f
Shimpali Deshpande <shimpali.deshpande@yahoo.com>
2018-09-19 22:03:00 +0530

    EditSmoothie Router

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="smoothie" class="edit-smoothie container">
+  <h2>Edit the {{smoothie.title}} Smoothie</h2>
+  </div>
+</template>
+
+<script>
+import db from '@/firebase/init';
+
+export default {
+	name: 'EditSmoothie',
+	data() {
+		return {
+      smoothie: null
+    };
+	},
+  created(){
+    let ref = db.collection('smoothies').where('slug', '==' , this.$route.params.smoothie_slug);
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        //console.log(doc.data());
+        this.smoothie = doc.data();
+        this.smoothie.id = doc.id;
+      });
+    });
+  }
+};
+</script>
+
+<style>
+.edit-smoothie{
+  margin-top: 60px;
+}
+</style>
